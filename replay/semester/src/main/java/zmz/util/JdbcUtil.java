54e4da70c6fdfd78082af1218435c631732a09d5
zmz <2726882386@qq.com>
2025-07-01 10:36:29 +0800

    学期和备课

---
+++ replay/semester/src/main/java/zmz/util/JdbcUtil.java
@@ -0,0 +1,33 @@
+package zmz.util;
+
+import java.sql.*;
+
+public class JdbcUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/?serverTimezone=Asia/Shanghai&useUnicode=true&characterEncoding=utf-8&useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+    // 释放所有资源
+    public static void close(Connection conn, Statement stmt, ResultSet rs) {
+        try { if (rs != null) rs.close(); } catch (Exception e) { e.printStackTrace(); }
+        try { if (stmt != null) stmt.close(); } catch (Exception e) { e.printStackTrace(); }
+        try { if (conn != null) conn.close(); } catch (Exception e) { e.printStackTrace(); }
+    }
+
+    // 只释放Connection和Statement
+    public static void close(Connection conn, Statement stmt) {
+        close(conn, stmt, null);
+    }
+}
